b2bd6058d10c7b398425ce3f30b70df202face0c
Asger Wenneberg <asger@wenneberg.net>
2025-12-10 19:54:13 +0100

    run faster 5x5 test case

--- csl/run_wse3.sh
+++ csl/run_wse3.sh
@@ -3,5 +3,5 @@
 set -e
 
 cslc --arch=wse3 ./layout.csl --fabric-dims=17,12 \
---fabric-offsets=4,1 --params=N_kernel:10,M_kernel:10,N_per_PE:25,max_nz_per_n:100 -o out --memcpy --channels 1
+--fabric-offsets=4,1 --params=N_kernel:5,M_kernel:5,N_per_PE:50,max_nz_per_n:100 -o out --memcpy --channels 1
 cs_python run.py --name out
